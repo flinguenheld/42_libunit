b2b0bc22e66fe42892100ef1948f41727e758df6
flinguen <florent@linguenheld.net>
2026-01-12 04:51:38 +0100

    Add limits.h in atol tests

--- real-tests/tests/atol/atol.h
+++ real-tests/tests/atol/atol.h
@@ -6,7 +6,7 @@
 /*   By: tghnassi <tghnassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 19:12:56 by flinguen          #+#    #+#             */
-/*   Updated: 2026/01/12 03:12:42 by flinguen         ###   ########.fr       */
+/*   Updated: 2026/01/12 04:48:36 by flinguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "../../../framework/libunit.h"
 # include "../../libft/libft.h"
+# include <limits.h>
 
 int	atol_launcher(t_count *final_count);
 int	atol_basic_test(void);
