5f36b3c96b18712952221c34c2c5d6eb3d64844a
tghnassi <tghnassi@student.42perpignan.fr>
2026-01-12 12:36:05 +0100

    make files messages suppresses, new implementation without framewok

--- framework/tests/signals/signals.h
+++ framework/tests/signals/signals.h
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       :::      ::::::::    */
+/*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flinguen <florent@linguenheld.net>          +#+  +:+       +#+       */
+/*   By: tghnassi <tghnassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 19:12:56 by flinguen          #+#    #+#             */
-/*   Updated: 2026/01/12 03:12:40 by flinguen         ###   ########.fr       */
+/*   Updated: 2026/01/12 12:30:20 by tghnassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
 
-# include "../framework/libunit.h"
+# include "../../libunit.h"
 
 int	signal_launcher(t_count *final_count);
 int	signal_ok_test(void);
