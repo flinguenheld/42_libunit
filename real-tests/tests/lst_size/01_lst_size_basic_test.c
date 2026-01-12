7b1419b0dab0fc80245a6c837438151db3d352c6
flinguen <florent@linguenheld.net>
2026-01-12 01:52:30 +0100

    Real test - lst size refactor tests

--- real-tests/tests/lst_size/01_lst_size_basic_test.c
+++ real-tests/tests/lst_size/01_lst_size_basic_test.c
@@ -6,7 +6,7 @@
 /*   By: tghnassi <tghnassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 19:20:47 by tghnassi          #+#    #+#             */
-/*   Updated: 2026/01/11 23:44:29 by tghnassi         ###   ########.fr       */
+/*   Updated: 2026/01/12 01:48:41 by flinguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,17 @@
 
 int	lst_size_basic_test(void)
 {
-	int 	i;
+	int		i;
+	int		is_ok;
 	t_list	*lst;
 
-	i = 5;
-	lst = ft_lst_new(&i);
+	i = 0;
+	lst = NULL;
+	ft_lst_push_front(&lst, ft_lst_new(NULL));
 	if (ft_lst_size(lst) == 1)
-	{
-		free(lst);
-		return (0);
-	}
+		is_ok = 0;
 	else
-	{
-		free(lst);
-		return (-1);
-	}
+		is_ok = -1;
+	ft_lst_clear_basic(&lst);
+	return (is_ok);
 }
--- real-tests/tests/lst_size/02_lst_size_large_test.c
+++ real-tests/tests/lst_size/02_lst_size_large_test.c
@@ -6,7 +6,7 @@
 /*   By: tghnassi <tghnassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 19:20:47 by tghnassi          #+#    #+#             */
-/*   Updated: 2026/01/12 01:27:26 by tghnassi         ###   ########.fr       */
+/*   Updated: 2026/01/12 01:48:41 by flinguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 int	lst_size_large_test(void)
 {
-	int 	i;
+	int		i;
+	int		is_ok;
 	t_list	*lst;
 
-	i = 9;
+	i = 0;
 	lst = NULL;
-	while (--i > 0)
+	while (i++ < 99999)
 		ft_lst_push_front(&lst, ft_lst_new(NULL));
-	if (ft_lst_size(lst) == 8)
-	{
-		ft_lst_clear_basic(&lst);
-		return (0);
-	}
+	if (ft_lst_size(lst) == 99999)
+		is_ok = 0;
 	else
-	{
-		ft_lst_clear_basic(&lst);
-		return (-1);
-	}
+		is_ok = -1;
+	ft_lst_clear_basic(&lst);
+	return (is_ok);
 }
